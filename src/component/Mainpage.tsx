f4d41faa1973ac3cf89012be666b565ce0bdce25
anishsaini <anishoficial@gmail.com>
2024-06-29 16:51:04 +0530

    completed our mainpage in which add search bar with the help of api

--- src/component/Mainpage.tsx
+++ src/component/Mainpage.tsx
@@ -1,9 +1,34 @@
-
-
+import './doc.css'
+import React, { useState } from'react'
+// import './Mealcard.tsx'
+import Mealcard from './Mealcard.tsx'
 const Mainpage = () => {
+
+    const [search, setSearch] = useState("")
+    const [count, setCount] = useState<Meal[]>([])
+    const inputvalue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value)
+    }
+
+    const callapi = async() => {
+        const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        const data = await get.json()
+        console.log(data.meals)
+        setCount(data.meals || [])
+    }
+    console.log(count)
+
   return (
-    <div>
-      this is mainpage
+    <div className="container">
+        <div className="searchBar">
+            <input type="text" placeholder="Enter Dish" onChange={inputvalue} />
+            <button onClick={callapi}>search</button>
+        </div>
+        <div>
+            {count.map((meal) => (
+                <Mealcard key={meal.idMeal} meal={meal} />
+            ))}
+        </div>
     </div>
   )
 }
---
+++ src/component/Mealcard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mealcard = () => {
+  return (
+    <div>
+      lkjcnk
+    </div>
+  )
+}
+
+export default Mealcard
